44c05f84775b885a9f19ce2669b3c2c92533430d
SilkFlowers <44918641+SilkFlowers@users.noreply.github.com>
2019-03-03 16:38:31 -0800

    Update main.cpp

--- simpcalc/simpcalc/main.cpp
+++ simpcalc/simpcalc/main.cpp
@@ -7,9 +7,11 @@
 //
 
 #include <iostream>
+//simple calculator with commands
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int main() {
+
+
     return 0;
 }
